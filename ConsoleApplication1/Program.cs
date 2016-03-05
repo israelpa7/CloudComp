fbcdedce0fae13af405827872ca8e047c16d5b88
israelpa7 <israelpa7@gmail.com>
2016-03-05 23:35:07 +0200

    remove redundent packages

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.SqlClient;
-using Microsoft.AspNet.Identity.EntityFramework;
-
 
 namespace ConsoleApplication1
 {
