75248c8bcaa2c544fb602155a6a3d8fcaa8ca0b5
israelpa7 <israelpa7@gmail.com>
2016-03-05 23:18:29 +0200

    try1

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = new List<Person>();
+            using (var dbContext = new qslEntities()) {
+                 res = dbContext.Person.ToList();
+            }
+
+            foreach (var item in res)
+            {
+                Console.WriteLine(string.Format("Id: {0}, First Name: {1}, Last Name: {2}", item.Id, item.FirstName, item.LastName));
+            }
+
+        }
+    }
+}
